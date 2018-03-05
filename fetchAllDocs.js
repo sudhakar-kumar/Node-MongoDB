dde326820334a161e89c44a2f976379ad9b2774c
Sudhakar Kumar Mandal <37034789+sudhakar-kumar@users.noreply.github.com>
2018-03-05 15:11:05 +0530

    Update fetchAllDocs.js
    Used '.project({_id:0})' at line 16. Just to hide that '_id' field from DB

--- fetchAllDocs.js
+++ fetchAllDocs.js
@@ -12,7 +12,8 @@ http.createServer(function(req,res){
 				
 			if(err) throw err;
 			var dbo=db.db("DB1");
-			dbo.collection("empData").find({},{_id:0}).toArray(function(err,result){
+			//dbo.collection("empData").find({},{_id:0}).toArray(function(err,result){ //here '{_id:0}' is not able to hide '_id' from DB
+			dbo.collection("empData").find({}).project({_id:0}).toArray(function(err,result){ //.project({_id:0}) works fine	
 				if (err) throw err;
 				console.log(result);
 				res.writeHead(200,{"Content-Type":"text/html"});
