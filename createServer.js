94155448e05eb1e744d8c56a22c7bc9a516036f0
Sudhakar Kumar Mandal <37034789+sudhakar-kumar@users.noreply.github.com>
2018-03-09 15:12:42 +0530

    Update createServer.js

--- createServer.js
+++ createServer.js
@@ -1,8 +1,10 @@
+//Creating a Server and listening on port 3000
+
 const http = require('http');
+//use 'const' for Not-changing-variables,otherwise 'var' is preferred
 
 http.createServer(function(req,res){
 	res.end("Server is up and running....");
 }).listen(3000);
 
 
-//creating a server and listening on port 3000
