749a70dcc3114d7a226386f90c1763c930a6aa4a
Sudhakar Kumar Mandal <37034789+sudhakar-kumar@users.noreply.github.com>
2018-06-28 17:00:05 +0530

    Update readXML.js

--- readXML.js
+++ readXML.js
@@ -1,13 +1,12 @@
-// Trying to read XML file
+// Read XML file
 
 var fs = require('fs'),
-    xml2js = require('xml2js'); // tab space should be maintained here
+    xml2js = require('xml2js'); // Tab space should be maintained !
 
 var path = require('path');
 var parser = new xml2js.Parser();
 
 fs.readFile(path.join(__dirname,'someRandom.xml'), function(err, data) {    
-        
     parser.parseString(data, function (err, result) {
         console.log(result);
         console.log(result.feedthisdata.data1);
